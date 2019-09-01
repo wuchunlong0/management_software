28807a423efb129c2b946d8fe1c89f4c6fe0c596
wuchunlong0 <wcl6005%40163.com>
2019-09-01 10:17:35 +0800

    add

---
+++ mysite/static/js/login.js
@@ -0,0 +1,39 @@
+
+/**
+* 用户登陆
+*/
+jQuery(document).ready(function($){
+	var $form_modal = $('.cd-user-modal'),
+		$form_login = $form_modal.find('#cd-login'),
+		$form_signup = $form_modal.find('#cd-signup'),
+		$form_modal_tab = $('.cd-switcher'),
+		$tab_login = $form_modal_tab.children('li').eq(0).children('a'),
+		$tab_signup = $form_modal_tab.children('li').eq(1).children('a'),
+		$runoob_pop = $('.runoob-pop');
+	
+
+	
+	//弹出窗口
+	$runoob_pop.on('click', function(event){
+		$form_modal.addClass('is-visible');	
+		//show the selected form
+		( $(event.target).is('.cd-signup') ) ? signup_selected() : login_selected();
+	});
+
+	//关闭弹出窗口
+	$('.cd-user-modal').on('click', function(event){
+		if( $(event.target).is($form_modal) || $(event.target).is('.cd-close-form') ) {
+			$form_modal.removeClass('is-visible');
+		}	
+	});
+	//使用Esc键关闭弹出窗口
+	$(document).keyup(function(event){
+    	if(event.which=='27'){
+    		$form_modal.removeClass('is-visible');
+	    }
+    });
+
+
+});
+
+
